8ceba59ed9a316c81a7d658bebb8c16fcdfd8b16
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-02-28 12:45:26 +0100

    Add project files.

---
+++ BitwiseOperatorsInCSharp/Calculator.cs
@@ -0,0 +1,88 @@
+﻿namespace BitwiseOperatorsInCSharp
+{
+    internal class Calculator
+    {
+        private double _operand1;
+        private double _operand2;
+        private char _op;
+        public double OperandOne {
+            get
+            { 
+                return _operand1;
+            }
+                
+            set 
+            {
+                _operand1 = value;
+            }
+        }
+        public double OperandTwo { 
+            get
+            { 
+                return _operand2;
+            } 
+            set 
+            { 
+                _operand2 = value;
+            } 
+        }
+        public char Op {
+            get
+            {
+                return _op;
+            }
+            set
+            {
+                _op = value;
+
+            }
+        }
+        
+        public bool BoolResult{ get; private set; }
+        public double Result { get; private set; }
+
+        public void MakeCalculation()
+        {
+            switch(Op)
+            {
+                case '+':   
+                    Result = OperandOne + OperandTwo;
+                    break;
+                case '-':
+                    Result = OperandTwo - OperandOne;
+                    break;
+                case '*':
+                    Result = OperandOne * OperandTwo;
+                    break;
+                case '/':
+                    Result = OperandTwo > 0 ? OperandOne / OperandTwo: double.NaN;
+                    break;
+                case '=':
+                    BoolResult = OperandOne == OperandTwo;
+                    break;
+                case '&':
+                    BoolResult = (OperandOne > 0) && (OperandTwo >0);
+                    break;
+                case '%':
+                    Result = OperandOne % OperandTwo;
+                    break;
+                case '>':
+                    BoolResult = OperandTwo > OperandOne;
+                    break;
+                case '<':
+                    BoolResult = OperandTwo < OperandOne;
+                    break;
+                case '!':
+                    BoolResult = (OperandOne !=0) && (OperandTwo !=0);
+                    break;
+                case '|':
+                    BoolResult = (OperandOne > 0) || (OperandTwo > 0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ BitwiseOperatorsInCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BitwiseOperatorsInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Building A Simple Calculator in C#...**");
+            Console.WriteLine();
+
+            Calculator cal = new Calculator();
+            Console.WriteLine("Enter the first Operand: ");
+            cal.OperandOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Second Operand : ");
+            cal.OperandTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Operator: (+, -, /, &, *, =, !, %, |, >, <)");
+            cal.Op  = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            cal.MakeCalculation();
+           Console.WriteLine("The result is {0} ", cal.Result);
+           Console.WriteLine("The  boolean result is {0} ", cal.BoolResult);
+        }
+    }
+}
