2434f4c8507863f59a4fbf82c5050316946f30ca
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-03-01 16:18:26 +0100

    updated my code

---
+++ BitwiseOperatorsInCSharp/MyCalculator.cs
@@ -0,0 +1,97 @@
+﻿namespace BitwiseOperatorsInCSharp
+{
+    internal class MyCalculator
+    {
+        private double _operand1;
+        private double _operand2;
+        private char _op;
+
+        public double OperandOne 
+        {
+            get 
+            {
+                return _operand1;
+            }
+            set 
+            { 
+                _operand1 = value;
+            }
+        }
+
+        public double OperandTwo
+        {
+            get
+            {
+                return _operand2;
+            }
+            set
+            {
+                _operand2 = value;
+            }
+        }
+
+        public char Op
+        {
+            get
+            {
+                return _op;
+            }
+            set
+            {
+                _op = value;
+            }
+        }
+
+        public double Result { get; private set; }
+        public bool BoolResult { get; private set; }
+
+        public  void MakeCalculation()
+        {
+            switch (Op)
+            {
+                case '+':
+                    Result = OperandOne + OperandTwo;
+                    break;
+                case '-':
+                    Result = OperandTwo - OperandOne;
+                    break;
+                case '*':
+                    Result = OperandTwo * OperandOne;
+                    break;
+                case '/':
+                    Result = OperandTwo >0 ? OperandOne/OperandTwo : double.NaN;
+                    break;
+                case '!':
+                    BoolResult = (OperandOne != 0) && (OperandTwo !=0);
+                    break;
+                case '|':
+                    BoolResult = (OperandTwo != 0) || (OperandOne != 0);
+                    break;
+                case '>':
+                    BoolResult = OperandOne > OperandTwo;
+                    break;
+                case '<':
+                    BoolResult = OperandOne < OperandTwo;
+                    break;
+                case '&':
+                    BoolResult = (OperandOne > 0) && (OperandTwo > 0);
+                    break;
+                case '%':
+                    Result = OperandOne % OperandTwo;
+                    break;
+                case '=':
+                    BoolResult = OperandOne == OperandTwo;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+            }
+            
+        }
+
+        
+
+          
+
+    }
+}
--- BitwiseOperatorsInCSharp/Program.cs
+++ BitwiseOperatorsInCSharp/Program.cs
@@ -4,20 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("**Building A Simple Calculator in C#...**");
+
+     
+            /*Console.WriteLine("**Building A Simple Calculator in C#...**");
             Console.WriteLine();
 
-            Calculator cal = new Calculator();
+            MyCalculator cal = new MyCalculator();
             Console.WriteLine("Enter the first Operand: ");
             cal.OperandOne = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the Second Operand : ");
             cal.OperandTwo = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the Operator: (+, -, /, &, *, =, !, %, |, >, <)");
-            cal.Op  = Console.ReadKey().KeyChar;
+            cal.Op = Console.ReadKey().KeyChar;
             Console.WriteLine();
             cal.MakeCalculation();
-           Console.WriteLine("The result is {0} ", cal.Result);
-           Console.WriteLine("The  boolean result is {0} ", cal.BoolResult);
+            Console.WriteLine("The result is {0} ", cal.Result);
+            Console.WriteLine("The  boolean result is {0} ", cal.BoolResult);*/
+
+            Console.WriteLine("**Building A Simple Calculator in C#...**");
+            Console.WriteLine();
+            
+        
+     Input: while (true)
+            {
+                try
+                {
+                        MyCalculator cal = new MyCalculator();
+                        Console.WriteLine("Enter the first Operand: ");
+                        cal.OperandOne = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter the Second Operand : ");
+                        cal.OperandTwo = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter the Operator: (+, -, /, &, *, =, !, %, |, >, <)");
+                        cal.Op = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        cal.MakeCalculation();
+                        Console.WriteLine("The result is {0} ", cal.Result);
+                        Console.WriteLine("The boolean result is {0} ", cal.BoolResult);
+                        Console.WriteLine();
+
+                        Console.WriteLine("Press Enter to continue or type 'exit' to quit.");
+                        string userInput = Console.ReadLine().Trim();
+                        if (userInput.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Exiting the program...");
+                            break;
+                        }
+                        goto Input;
+                }
+             
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                    goto Input;
+                }
+            }
         }
     }
 }
